ddbc0c6c3b66d7247fc891dc34bcfcf58883c6d9
peverall <53804819+peverall@users.noreply.github.com>
2024-09-06 02:40:59 -0700

    Made minor changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    experimental: {
-      appDir: true, // Enable the App Router
-    },
-  };
-  
-  export default nextConfig;
+const nextConfig = {};
+
+export default nextConfig;
+
   
